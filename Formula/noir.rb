aef5232855dee44a13c975795bf2bfa8ad530347
Giovanni Barillari <giovanni.barillari@gmail.com>
2022-04-21 01:22:20 +0200

    Update noir formula

--- Formula/noir.rb
+++ Formula/noir.rb
@@ -1,16 +1,16 @@
 class Noir < Formula
     desc "CLI to Renoir templating engine"
     homepage "github.com/gi0baro/noir"
-    version "0.3.3"
+    version "0.3.4"
     license "BSD-3-Clause"
 
     if OS.mac?
-        url "https://github.com/gi0baro/noir/releases/download/0.3.3/noir-0.3.3-darwin-x86_64.tar.gz"
-        sha256 "58b4c85bd0945465def388e4ff9b79d8429b32251edcddb935532025d1faef04"
+        url "https://github.com/gi0baro/noir/releases/download/0.3.4/noir-0.3.4-darwin-x86_64.tar.gz"
+        sha256 "758882b30a4ddddcca7ef1182da0d5d617d33bb97820056bab51ade480b47426"
     end
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/gi0baro/noir/releases/download/0.3.3/noir-0.3.3-linux-x86_64.tar.gz"
-        sha256 "c8969dd5c9288898c1a6eb09e10d215270689afbf2e4c4e87487ed4a83e6dff4"
+        url "https://github.com/gi0baro/noir/releases/download/0.3.4/noir-0.3.4-linux-x86_64.tar.gz"
+        sha256 "86bcc628c76a5aa787b7b35cf9f4421f4e79ac7e175e372eabf7361376edc28d"
     end
 
     def install
