e0831935b73cae7e00a214f7301fcc49d750cb20
Giovanni Barillari <giovanni.barillari@gmail.com>
2022-09-05 02:11:50 +0200

    Update poetry-bin formula

--- Formula/poetry-bin.rb
+++ Formula/poetry-bin.rb
@@ -1,23 +1,26 @@
 class PoetryBin < Formula
     desc "Python package management tool (binary build)"
     homepage "github.com/gi0baro/poetry-bin"
-    version "1.1.15"
+    version "1.2.0"
     revision 1
     license "MIT"
 
     if OS.mac?
-        url "https://github.com/gi0baro/poetry-bin/releases/download/1.1.15-1/poetry-bin-1.1.15-1-x86_64-apple-darwin.tar.gz"
-        sha256 "9903c8809bfa57043b3bb624cd24713ff61dc7609ab9afd1ad824056aa25ddea"
+        url "https://github.com/gi0baro/poetry-bin/releases/download/1.2.0-1/poetry-bin-1.2.0-1-x86_64-apple-darwin.tar.gz"
+        sha256 "e859356641cea53a2b8da5a9a8ecd66fc103e44c85822c1cac8b49b5005e04d0"
     end
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/gi0baro/poetry-bin/releases/download/1.1.15-1/poetry-bin-1.1.15-1-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "928221fbb1518be4dc63f12424412efc0c446c4cc34e32a8e3919fae8489cc63"
+        url "https://github.com/gi0baro/poetry-bin/releases/download/1.2.0-1/poetry-bin-1.2.0-1-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "21495bef67bb4ead72a6e1742ee7e7db5c22445ffa771cde01fa8ef683d18da9"
     end
 
     def install
-        bin.install "bin/poetry"
+        bin.install "poetry"
         lib.install Dir["lib/*"]
         prefix.install Dir["assets"]
+
+        bin.install_symlink lib
+        bin.install_symlink prefix/"assets"
     end
 
     test do
---
+++ Formula/poetry-bin@1.1.rb
@@ -0,0 +1,28 @@
+class PoetryBinAT11 < Formula
+    desc "Python package management tool (binary build)"
+    homepage "github.com/gi0baro/poetry-bin"
+    version "1.1.15"
+    revision 1
+    license "MIT"
+
+    if OS.mac?
+        url "https://github.com/gi0baro/poetry-bin/releases/download/1.1.15-1/poetry-bin-1.1.15-1-x86_64-apple-darwin.tar.gz"
+        sha256 "9903c8809bfa57043b3bb624cd24713ff61dc7609ab9afd1ad824056aa25ddea"
+    end
+    if OS.linux? && Hardware::CPU.intel?
+        url "https://github.com/gi0baro/poetry-bin/releases/download/1.1.15-1/poetry-bin-1.1.15-1-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "928221fbb1518be4dc63f12424412efc0c446c4cc34e32a8e3919fae8489cc63"
+    end
+
+    keg_only :versioned_formula
+
+    def install
+        bin.install "bin/poetry"
+        lib.install Dir["lib/*"]
+        prefix.install Dir["assets"]
+    end
+
+    test do
+        system "#{bin}/poetry --version"
+    end
+end
