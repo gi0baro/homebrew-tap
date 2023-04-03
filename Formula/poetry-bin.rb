fd6a66e3cc9e9f2b6da0fc271d955a8878a7474a
Giovanni Barillari <g@baro.dev>
2023-04-03 14:41:27 +0200

    Update poetry-bin formula

--- Formula/poetry-bin.rb
+++ Formula/poetry-bin.rb
@@ -1,21 +1,21 @@
 class PoetryBin < Formula
     desc "Python package management tool (binary build)"
     homepage "github.com/gi0baro/poetry-bin"
-    version "1.4.1"
+    version "1.4.2"
     revision 1
     license "MIT"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/gi0baro/poetry-bin/releases/download/1.4.1-1/poetry-bin-1.4.1-1-x86_64-apple-darwin.tar.gz"
-        sha256 "c34785f6ad8b044cd320103de00eb63403689741a4fabec20ea2066a77fb9e6e"
+        url "https://github.com/gi0baro/poetry-bin/releases/download/1.4.2-1/poetry-bin-1.4.2-1-x86_64-apple-darwin.tar.gz"
+        sha256 "d720d4643fe3d713126e526b61046570418fe1a354172896efaeef2fc37b5b26"
     end
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/gi0baro/poetry-bin/releases/download/1.4.1-1/poetry-bin-1.4.1-1-aarch64-apple-darwin.tar.gz"
-        sha256 "75b287dcd39bbaccf74b92d21523fef16e13f1fe8af198da51556965608ec077"
+        url "https://github.com/gi0baro/poetry-bin/releases/download/1.4.2-1/poetry-bin-1.4.2-1-aarch64-apple-darwin.tar.gz"
+        sha256 "0f10f02e2170b025f729c0592451c197323fb1da70626c880afa99d908093b0d"
     end
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/gi0baro/poetry-bin/releases/download/1.4.1-1/poetry-bin-1.4.1-1-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "a4e66b55ee26ca1e23d7fe823f1e103afcd29dddacc26fc56d9ed1ab086ee87b"
+        url "https://github.com/gi0baro/poetry-bin/releases/download/1.4.2-1/poetry-bin-1.4.2-1-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "60a54a58f70b2e3e7d8ec4e18a0a0c3ad62ed5769d89f248169964316fa378b0"
     end
 
     def install
