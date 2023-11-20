569d93c58e5f3a73766d0015a218bc0b1cb2ba73
Giovanni Barillari <g@baro.dev>
2023-11-20 18:15:47 +0100

    Update poetry-bin formula

--- Formula/poetry-bin.rb
+++ Formula/poetry-bin.rb
@@ -1,25 +1,25 @@
 class PoetryBin < Formula
     desc "Python package management tool (binary build)"
     homepage "github.com/gi0baro/poetry-bin"
-    version "1.7.0"
+    version "1.7.1"
     revision 1
     license "MIT"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/gi0baro/poetry-bin/releases/download/1.7.0-1/poetry-bin-1.7.0-1-x86_64-apple-darwin.tar.gz"
-        sha256 "0fa8583de7ac256182be03d2346a7909afe6979558e14a26a04a6d5726977005"
+        url "https://github.com/gi0baro/poetry-bin/releases/download/1.7.1-1/poetry-bin-1.7.1-1-x86_64-apple-darwin.tar.gz"
+        sha256 "8a310b011dab2e2cd8f7cdadc62c59643e8f936667fccc904b21a11e76f79e92"
     end
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/gi0baro/poetry-bin/releases/download/1.7.0-1/poetry-bin-1.7.0-1-aarch64-apple-darwin.tar.gz"
-        sha256 "b529726b5d32cabd353f09da31b680059868d80e46dfeb5cc83c75e4d0ae969d"
+        url "https://github.com/gi0baro/poetry-bin/releases/download/1.7.1-1/poetry-bin-1.7.1-1-aarch64-apple-darwin.tar.gz"
+        sha256 "09551d92d1aae03de7f129e4e8185b6884b783e34aa8475f84e599d909f732ef"
     end
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/gi0baro/poetry-bin/releases/download/1.7.0-1/poetry-bin-1.7.0-1-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "03ee204da7cd2161e6a7ae6041b129f9f1f62ec713d42879babfcc8ee22b05c6"
+        url "https://github.com/gi0baro/poetry-bin/releases/download/1.7.1-1/poetry-bin-1.7.1-1-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "bea7be56b37f5e044d79858ccbdd26d4d0879998bbc8b63f2e7ce3797eb30128"
     end
     if OS.linux? && Hardware::CPU.arm?
-        url "https://github.com/gi0baro/poetry-bin/releases/download/1.7.0-1/poetry-bin-1.7.0-1-aarch64-unknown-linux-gnu.tar.gz"
-        sha256 "8cb1d92e6bd2e0a7604d38e3efe3d875c3d37a1d45230fe8f2517b6832a622f7"
+        url "https://github.com/gi0baro/poetry-bin/releases/download/1.7.1-1/poetry-bin-1.7.1-1-aarch64-unknown-linux-gnu.tar.gz"
+        sha256 "7580b5f96e9f12ee89c2626bf995fd614445e6ba4dd65a3bd866dfd329cacf66"
     end
 
     def install
